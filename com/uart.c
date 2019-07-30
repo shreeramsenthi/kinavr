ad5ad790bf55742738696fe17cd6c39e4616ff39
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2019-07-30 11:45:49 -0700

    Add timing code

---
+++ com/uart.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+
+const int tx_pin = PIN2_bm; // Port B
+const uint16_t uart_baudrate = 1388; //Specific to 3.33MHz
+
+void uart_init (){
+  // Set up Tx pin
+  PORTB_DIRSET = tx_pin;
+  PORTB_OUTSET = tx_pin;
+
+  // Enable UART
+  USART0_BAUD = uart_baudrate;// set baud rate
+  USART0_CTRLB = USART_TXEN_bm; // Enable Tx Pin
+  USART0_CTRLC = USART_SBMODE_2BIT_gc | USART_CHSIZE_8BIT_gc; // Set 2 stop bits and 8bit messages
+}
+
+void uart_transmit (unsigned char data){
+  while(!(USART0_STATUS & USART_DREIF_bm)); // Wait for old data to finish transmitting
+  USART0_TXDATAL = data; // put data byte into the transfer register
+}
+
+void uart_print_byte_hex (unsigned char data) {
+  uint8_t msd = data / 16 + 48; // First digit in hex, shifted to 0 in ASCII
+  uint8_t lsd = data % 16 + 48; // Second digit
+
+  if (msd > 57) // shift values from A-F to ASCII A
+    msd += 7;
+  if (lsd > 57)
+    lsd += 7;
+
+  uart_transmit(msd);
+  uart_transmit(lsd);
+}
---
+++ main.c
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "com/uart.c"
+#include "com/spi.c"
+
+#include "modes/dc.c"
+#include "modes/do.c"
+
+void timer_init () {
+  TCA0_SINGLE_PER = 3255; // 3.33MHz / 1024 = 3255 for 1 second period
+  TCA0_SINGLE_CTRLA |= (TCA_SINGLE_CLKSEL_DIV1024_gc) | (TCA_SINGLE_ENABLE_bm);
+  TCA0_SINGLE_INTCTRL |= (TCA_SINGLE_OVF_bm); // Enable timer interrupts on overflow on timer A
+}
+
+ISR(TCA0_OVF_vect)
+{
+  uart_transmit('H');
+  uart_transmit('I');
+  uart_transmit(10);
+  uart_transmit(13);
+
+  TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm;
+}
+
+int main () {
+  timer_init();
+  sei();
+
+  while(1);
+}
