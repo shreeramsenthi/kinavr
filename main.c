0cc0732ac44b3ec5352e11a09d207f358d089c35
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2019-07-31 09:39:35 -0700

    Test button ISR

--- main.c
+++ main.c
@@ -7,24 +7,31 @@
 #include "modes/dc.c"
 #include "modes/do.c"
 
-void timer_init () {
-  TCA0_SINGLE_PER = 3255; // 3.33MHz / 1024 = 3255 for 1 second period
-  TCA0_SINGLE_CTRLA |= (TCA_SINGLE_CLKSEL_DIV1024_gc) | (TCA_SINGLE_ENABLE_bm);
-  TCA0_SINGLE_INTCTRL |= (TCA_SINGLE_OVF_bm); // Enable timer interrupts on overflow on timer A
-}
+const int DO_BTN = PIN5_bm; // PORT A
+const int DC_BTN = PIN6_bm; // PORT A
 
-ISR(TCA0_OVF_vect)
-{
-
-  TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm;
+ISR (PORTA_PORT_vect) {
+  uart_print_byte_hex(PORTA_INTFLAGS);
+  PORTA_PIN5CTRL = 0;
+  PORTA_PIN6CTRL = 0;
+  PORTA_INTFLAGS |= DO_BTN | DC_BTN;
 }
 
 int main () {
-  //// Timer Initialization
-  //timer_init();
-  //sei();
+  // Mode button setup
+  PORTA_DIRCLR = DO_BTN | DC_BTN;
+  PORTA_PIN5CTRL = PORT_PULLUPEN_bm | PORT_ISC_FALLING_gc;
+  PORTA_PIN6CTRL = PORT_PULLUPEN_bm | PORT_ISC_FALLING_gc;
+  sei();
 
-  output_data();
+  // Testing
+  uart_init();
+  uart_transmit('X');
+  uart_transmit(10);
+  uart_transmit(13);
+
+  //output_data();
+  //collect_data();
 
   while(1);
 }
---
+++ modes/dc.c
@@ -0,0 +1,27 @@
+
+/*------------------------------------------*/
+/*                 Timing                   */
+/*------------------------------------------*/
+
+void timer_init () {
+  TCA0_SINGLE_PER = 3255; // 3.33MHz / 1024 = 3255 for 1 second period
+  TCA0_SINGLE_CTRLA |= (TCA_SINGLE_CLKSEL_DIV1024_gc) | (TCA_SINGLE_ENABLE_bm);
+  TCA0_SINGLE_INTCTRL |= (TCA_SINGLE_OVF_bm); // Enable timer interrupts on overflow on timer A
+}
+
+ISR(TCA0_OVF_vect)
+{
+
+  TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm;
+}
+
+/*------------------------------------------*/
+/*                  Main                    */
+/*------------------------------------------*/
+
+void collect_data() {
+  // Timer Initialization
+  timer_init();
+
+  while(1);
+}
