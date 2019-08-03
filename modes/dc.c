d458db48436bbca800749e2d82327b7fed003056
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2019-08-03 00:49:11 -0400

    Test 2kHz sampling

--- modes/dc.c
+++ modes/dc.c
@@ -1,12 +1,11 @@
 volatile uint32_t cur_addr = 0;
-volatile _Bool ready = 1;
 
 /*------------------------------------------*/
 /*                 Timing                   */
 /*------------------------------------------*/
 
 void timer_init () {
-  TCA0_SINGLE_PER = 32000; // 16MHz clock / 500Hz sampling rate
+  TCA0_SINGLE_PER = 8000; // 16MHz clock / 500Hz sampling rate
   TCA0_SINGLE_CTRLA = TCA_SINGLE_ENABLE_bm;
   TCA0_SINGLE_INTCTRL |= TCA_SINGLE_OVF_bm; // Enable timer interrupts on overflow on timer A
   sei();
@@ -24,7 +23,7 @@ ISR(TCA0_OVF_vect)
     if(cur_addr > (max_addr - 0x12)) // if next address is within 18 bytes of end
       cli(); // clear global interrupts (stop sampling)
 
-	if((TCA0_SINGLE_INTFLAGS & TCA_SINGLE_OVF_bm) | cur_addr > 0x3CFF) {
+	if((TCA0_SINGLE_INTFLAGS & TCA_SINGLE_OVF_bm) | cur_addr >= 0x7C00) {
 		TCA0_SINGLE_CTRLA = 0;
 		uart_transmit('q');
 		uart_print_byte_hex((cur_addr >> 16) & 0xFF);
