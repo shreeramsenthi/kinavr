a053d430835417c0f3004437f3a2b91facbafccd
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2019-08-02 22:26:03 -0700

    Test address update in dc

--- modes/dc.c
+++ modes/dc.c
@@ -5,31 +5,31 @@ volatile uint32_t cur_addr = 0;
 /*------------------------------------------*/
 
 void timer_init () {
-  TCA0_SINGLE_PER = 10000; // 16MHz clock / 500Hz sampling rate
+  TCA0_SINGLE_PER = 10000; // 16MHz clock / 1.6kHz sampling rate
   TCA0_SINGLE_CTRLA = TCA_SINGLE_ENABLE_bm;
-  TCA0_SINGLE_INTCTRL |= TCA_SINGLE_OVF_bm; // Enable timer interrupts on overflow on timer A
+  TCA0_SINGLE_INTCTRL |= TCA_SINGLE_CLKSEL_DIV64_gc |
+    TCA_SINGLE_OVF_bm; // Enable timer interrupts on overflow on timer A
   sei();
 }
 
 ISR(TCA0_OVF_vect)
 {
-    TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm;
+  TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm;
 
-    sensor_sample(SS_SEN0, cur_addr);
-    cur_addr += 18;
-    if((cur_addr % 0xFF) % 0xFC == 0) // skip last 4 bytes of each page
-      cur_addr += 3;
+  uart_print_byte_hex(cur_addr & 0xFF); // testing
 
-    if(cur_addr > (max_addr - 0x12)) // if next address is within 18 bytes of end
-      TCA0_SINGLE_CTRLA = 0; // stop sampling
+  sensor_sample(SS_SEN0, cur_addr);
+  cur_addr += 18;
+  if((cur_addr % 0xFF) % 0xFC == 0) // skip last 4 bytes of each page
+    cur_addr += 3;
 
-	if((TCA0_SINGLE_INTFLAGS & TCA_SINGLE_OVF_bm) | cur_addr >= 0x9E12) {
-		TCA0_SINGLE_CTRLA = 0;
-		uart_transmit('q');
-		uart_print_byte_hex((cur_addr >> 16) & 0xFF);
-		uart_print_byte_hex((cur_addr >> 8) & 0xFF);
-		uart_print_byte_hex((cur_addr >> 0) & 0xFF);
-	}
+  if(cur_addr > (max_addr - 0x12)) { // if next address is within 18 bytes of end
+    TCA0_SINGLE_CTRLA = 0; // stop sampling
+    TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm; // clear interrupt flags again
+  }
+
+  if(TCA0_SINGLE_INTFLAGS & TCA_SINGLE_OVF_bm) // If ISR is called too frequently
+    TCA0_SINGLE_CTRLA = 0; // stop sampling
 }
 
 /*------------------------------------------*/
