cb6e98f8691d913cea6c50c38f965accfb7c2e41
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2019-08-02 23:17:58 -0700

    Revert to working 1.6kHz sampling

--- modes/dc.c
+++ modes/dc.c
@@ -6,7 +6,7 @@ volatile uint32_t cur_addr = 0;
 
 void timer_init () {
   TCA0_SINGLE_PER = 10000; // 16MHz clock / 500Hz sampling rate
-  TCA0_SINGLE_CTRLA = TCA_SINGLE_CLKSEL_DIV64_gc | TCA_SINGLE_ENABLE_bm;
+  TCA0_SINGLE_CTRLA = TCA_SINGLE_ENABLE_bm;
   TCA0_SINGLE_INTCTRL |= TCA_SINGLE_OVF_bm; // Enable timer interrupts on overflow on timer A
   sei();
 }
@@ -14,8 +14,6 @@ void timer_init () {
 ISR(TCA0_OVF_vect)
 {
     TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm;
-    uart_print_byte_hex(cur_addr & 0xFF);
-    uart_transmit(13);
 
     sensor_sample(SS_SEN0, cur_addr);
     cur_addr += 18;
@@ -25,10 +23,13 @@ ISR(TCA0_OVF_vect)
     if(cur_addr > (max_addr - 0x12)) // if next address is within 18 bytes of end
       TCA0_SINGLE_CTRLA = 0; // stop sampling
 
-  if((TCA0_SINGLE_INTFLAGS & TCA_SINGLE_OVF_bm) | cur_addr >= 0x9E12) {
-    TCA0_SINGLE_CTRLA = 0;
-    uart_transmit('q');
-  }
+	if((TCA0_SINGLE_INTFLAGS & TCA_SINGLE_OVF_bm) | cur_addr >= 0x9E12) {
+		TCA0_SINGLE_CTRLA = 0;
+		uart_transmit('q');
+		uart_print_byte_hex((cur_addr >> 16) & 0xFF);
+		uart_print_byte_hex((cur_addr >> 8) & 0xFF);
+		uart_print_byte_hex((cur_addr >> 0) & 0xFF);
+	}
 }
 
 /*------------------------------------------*/
