795453d455a57a5f05a82b6726fc5108cde2cd63
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2019-08-02 20:05:47 -0700

    Time sampling code

--- modes/dc.c
+++ modes/dc.c
@@ -13,14 +13,25 @@ void timer_init () {
 
 ISR(TCA0_OVF_vect)
 {
-  //sensor_sample(SS_SEN0, cur_addr);
-  uart_transmit('E'); // testing
+  //testing
+  uint16_t start_time = TCA0_SINGLE_CNT;
+  uart_print_byte_hex(start_time >> 8);
+  uart_print_byte_hex(start_time & 0xFF);
+
+  sensor_sample(SS_SEN0, cur_addr);
   cur_addr += 18;
   if((cur_addr % 0xFF) % 0xFC == 0) // skip last 4 bytes of each page
     cur_addr += 3;
 
   if(cur_addr > (max_addr - 0x12)) // if next address is within 18 bytes of end
     cli(); // clear global interrupts (stop sampling)
+
+  //testing
+  uint16_t end_time = TCA0_SINGLE_CNT;
+  uart_print_byte_hex(end_time >> 8);
+  uart_print_byte_hex(end_time & 0xFF);
+  uart_transmit(10);
+  uart_transmit(13);
 
   TCA0_SINGLE_INTFLAGS = TCA_SINGLE_OVF_bm;
 }
@@ -34,13 +45,13 @@ void collect_data() {
   uart_init(); // testing
   spi_init();
 
-  //sensor_init(SS_SEN0);
+  sensor_init(SS_SEN0);
 
-  //// Setup memory
-  //flash_chip_erase ();
-  //flash_wait_for_ready();
+  // Setup memory
+  flash_chip_erase ();
+  flash_wait_for_ready();
 
-  //while(!(~PORTA_IN & PIN6_bm));
+  while(!(~PORTA_IN & PIN6_bm));
 
   // Start sampling
   timer_init();
