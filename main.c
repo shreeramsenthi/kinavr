e734c2c1621d80bf322a5c01ecca9797a5b826d4
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2019-12-24 10:51:52 -0800

    Test new mag switch

--- main.c
+++ main.c
@@ -4,31 +4,41 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+const int LED_PIN = PIN4_bm;    // PORT A
+const int SWITCH_PIN = PIN7_bm; // PORT A
+
 #include "com/uart.c"
 #include "com/spi.c"
 
 #include "modes/do.c"
 #include "modes/dc.c"
 
-const int DC_BTN = PIN5_bm; // PORT A
-const int DO_BTN = PIN6_bm; // PORT A
-
 int main () {
   // Setup master clock speed
   CPU_CCP = CCP_IOREG_gc; // Unlock protected io regs
   CLKCTRL_MCLKCTRLB = 0; // set clock speed to 16MHz
 
-  // Mode button setup
-  PORTA_DIRCLR = DO_BTN | DC_BTN;
-  PORTA_PIN5CTRL = PORT_PULLUPEN_bm;
-  PORTA_PIN6CTRL = PORT_PULLUPEN_bm;
+  // Hall effect switch setup
+  PORTA_DIRCLR = SWITCH_PIN;
+  PORTA_PIN7CTRL = PORT_PULLUPEN_bm;
+
+  // LED setup
+  PORTA_DIRSET = LED_PIN;
+  PORTA_OUTSET = LED_PIN;
 
   while(1){
-    if (~PORTA_IN & DC_BTN) { // Button press grounds the pin, pull-up resistor is enabled otherwise
-      collect_data();
+    if (PORTA_IN & SWITCH_PIN) { // check if switch is high
+      PORTA_OUTCLR = LED_PIN;
+      _delay_ms(5000);
+      if(~PORTA_IN & SWITCH_PIN) // if switched to low
+        PORTA_OUTSET = LED_PIN;
+        //collect_data();
+      else
+        PORTA_OUTCLR = LED_PIN;
+        //output_data();
+    _delay_ms(5000);
+    PORTA_OUTSET = LED_PIN;
     }
-    if (~PORTA_IN & DO_BTN) {
-      output_data();
-    }
+    _delay_ms(1000);
   }
 }
